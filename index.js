4726ee6b3e1fb9d152144ecf840d6a45cc47f647
Sam Wen <samw@ritani.com>
2022-07-30 19:59:59 -0400

    remove inputs cluster options

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 'use strict';
 
 const { execSync } = require('child_process');
-const core = require('@actions/core');
 const install_redis = require('./utils/install-redis');
 const setup_cluster = require('./utils/setup-cluster');
 
 (async() => {
     try {
-        let cluster = true;
-        if (core.getInput('cluster') === 'false') cluster = false;
-        console.log('install redis', {cluster});
         install_redis();
-        if (cluster) {
-            console.log('setup redis-cluster');
-            setup_cluster();
-        }
+        setup_cluster();
         execSync('redis-server -v', {stdio: 'inherit'});
     } catch (error) {
         core.setFailed(error.message);
